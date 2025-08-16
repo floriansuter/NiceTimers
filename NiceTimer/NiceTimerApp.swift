459f209139daf44621cdc4f076dddf807e9f36d7
Florian Suter <florian.h.suter@gmail.com>
2025-08-16 16:44:19 +0200

    Initial Commit

---
+++ NiceTimer/NiceTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceTimerApp.swift
+//  NiceTimer
+//
+//  Created by Florian Suter on 16.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct NiceTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
